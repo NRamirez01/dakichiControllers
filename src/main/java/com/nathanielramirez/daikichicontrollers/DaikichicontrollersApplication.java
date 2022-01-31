6ba1b4d483fdee77a9da21c8ad1276b3681b7f04
Nathaniel Ramirez <nramire01@gmail.com>
2022-01-30 21:07:54 -0600

    commit

---
+++ src/main/java/com/nathanielramirez/daikichicontrollers/DaikichicontrollersApplication.java
@@ -0,0 +1,13 @@
+package com.nathanielramirez.daikichicontrollers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichicontrollersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichicontrollersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathanielramirez/daikichicontrollers/DaikichicontrollersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanielramirez.daikichicontrollers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichicontrollersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
